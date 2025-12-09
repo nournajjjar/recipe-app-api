97e36ca0421bbe2bfc442242451add6166a0f0db
nournajjjar <nourelhouda.najjar@esprit.tn>
2025-12-09 16:40:31 +0100

    egfccssssce

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,5 +1,5 @@
 """
-Django command to wait for the database to be available
+Django command to wait for the database to be available.
 """
 import time
 from psycopg2 import OperationalError as Psycopg2OpError
@@ -9,7 +9,7 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django command to wait for database."""
+    """Django command to wait for the database."""
 
     def handle(self, *args, **options):
         """Entrypoint for command."""
