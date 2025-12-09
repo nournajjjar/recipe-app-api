40c3f62a9354f18a607a1f952dca2f801fed1180
nournajjjar <nourelhouda.najjar@esprit.tn>
2025-12-09 11:53:01 +0100

    updates

---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+"""sample tests"""
+from django.test import SimpleTestCase
+from app import calc
+
+class CaltcTests(SimpleTestCase):
+    """rest the calc module"""
+    def test_add_numbers(self):
+        """test adding numbers toghether"""
+        res = calc.add(5,6)
+        self.assertEqual(res, 11)
+    def test_subtract_numbers(self):
+        """test subtracting  numbers"""
+        res = calc.subtract(10,15)
+        self.assertEqual(res, 5)
