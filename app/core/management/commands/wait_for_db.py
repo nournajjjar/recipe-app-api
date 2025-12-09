134b9a9618120d7dd30a0d7a721f742d9dd3c803
nournajjjar <nourelhouda.najjar@esprit.tn>
2025-12-09 16:10:04 +0100

    updates

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,6 @@
+""" Django command to wait for the db to be available"""
+from django.core.management.base import BaseCommand
+class Command ( BaseCommand):
+    """"django command to wait for db"""
+    def handle(self, *args, **options):
+        pass 
